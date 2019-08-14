5bec94b43714374fde5619f150da1a08b5a247dc
sanlushibawan <drizzle3015@outlook.com>
2019-08-14 17:46:45 +0800

    添加页面，为调用api准备(今天暂时没有思路)

--- app/src/main/java/com/drizzle/learnapplication/MainActivity.java
+++ app/src/main/java/com/drizzle/learnapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.drizzle.learnapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,14 +25,16 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity{
-    private static final String URL="https://api.androidhive.info/volley/person_object.json";
+public class MainActivity extends AppCompatActivity {
+    private static final String URL = "https://api.androidhive.info/volley/person_object.json";
     @BindView(R.id.set_btn)
     Button setBtn;
     @BindView(R.id.get_bean)
     Button getBean;
     @BindView(R.id.to_frameLayout)
     Button toFrameLayout;
+    @BindView(R.id.to_test_driver)
+    Button toTestDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +44,13 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    @OnClick({R.id.set_btn, R.id.get_bean, R.id.to_frameLayout})
+    @OnClick({R.id.set_btn, R.id.get_bean, R.id.to_frameLayout,R.id.to_test_driver})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.set_btn:
-                Log.d("MainActivity","registrationID="+JPushInterface.getRegistrationID(getApplicationContext()));
-                if (JPushInterface.getRegistrationID(getApplicationContext())!=null){
-                    JPushInterface.setAlias(getApplicationContext(),2,"我是别名哦");
+                Log.d("MainActivity", "registrationID=" + JPushInterface.getRegistrationID(getApplicationContext()));
+                if (JPushInterface.getRegistrationID(getApplicationContext()) != null) {
+                    JPushInterface.setAlias(getApplicationContext(), 2, "我是别名哦");
                 }
                 break;
             case R.id.get_bean:
@@ -57,31 +60,36 @@ public class MainActivity extends AppCompatActivity{
             case R.id.to_frameLayout:
                 //这里用来练习，framelayout
                 break;
+            case R.id.to_test_driver:
+                //驾照考试界面
+                Intent intent=new Intent(this,SecondActivity.class);
+                startActivity(intent);
+                break;
         }
     }
 
-    private void jsonGet(){
-        OkHttpClient client=new OkHttpClient();
-        Request request =new Request.Builder()
+    private void jsonGet() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
                 .url(URL)
                 .build();
-        Call call= client.newCall(request);
+        Call call = client.newCall(request);
         call.enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Log.d("MainActivity","error="+e.toString());
+                Log.d("MainActivity", "error=" + e.toString());
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                String responseData =response.body().string();
-                Gson gson=new Gson();
-                MyJavabean myJavabean=gson.fromJson(responseData,MyJavabean.class);
-                MyJavabean.Phone phone=myJavabean.getPhone();
-                Log.d("MainActivity","name="+myJavabean.getName());
-                Log.d("MainActivity","email="+myJavabean.getEmail());
-                Log.d("MainActivity","home="+phone.getHome());
-                Log.d("MainActivity","mobile="+phone.getMobile());
+                String responseData = response.body().string();
+                Gson gson = new Gson();
+                MyJavabean myJavabean = gson.fromJson(responseData, MyJavabean.class);
+                MyJavabean.Phone phone = myJavabean.getPhone();
+                Log.d("MainActivity", "name=" + myJavabean.getName());
+                Log.d("MainActivity", "email=" + myJavabean.getEmail());
+                Log.d("MainActivity", "home=" + phone.getHome());
+                Log.d("MainActivity", "mobile=" + phone.getMobile());
             }
         });
         //使用lambda表达式 替换 new Runnable()
---
+++ app/src/main/java/com/drizzle/learnapplication/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.drizzle.learnapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @BindView(R.id.driver_text)
+    TextView driverText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+        getApi();
+    }
+    private void getApi(){
+
+    }
+}
